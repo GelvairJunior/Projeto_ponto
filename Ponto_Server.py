a0e4472db292e2af500c2948aece5a4462059e8d
GelvairJunior <gelvair.schumann.jr@gmail.com>
2025-05-19 07:38:01 -0300

    Atualizando_com_POO
    Aqui apliquei mais algumas atualizações, como arrumar o código para ficar mais proximo da Orientação a Objetos e preparei um pouco mais do visual

--- Ponto_Server.py
+++ Ponto_Server.py
@@ -3,45 +3,61 @@ import cadastro
 
 
 class Ponto_Server():
+    
+    servidor = cadastro.Server()
+    sistema = cadastro.Funcionario(servidor)
 
-    def __init__(self):
-        servidor = cadastro.Server()
-        sistema = cadastro.Funcionario(servidor)
+    window = Tk()
+    window.geometry("600x600")
+    window.title("Pagina de controle de informação dos funcionarios ")
+    window.config(background="#2545d3")
 
-        window = Tk()
-        window.geometry("600x600")
-        window.title("Pagina de controle de informação dos funcionarios ")
-        window.config(background="#2545d3")
+    label = Label(window, text='Escolha uma opção: ')
+    label.pack()
 
-        label = Label(window, text='Escolha uma opção: ')
-        label.pack()
-
-        button_Cadastro = Button(window, text="Cadastrar Funcionario")
-        button_Cadastro.pack()
+    button_Cadastro = Button(window, text="Cadastrar Funcionario")
+    button_Cadastro.pack()
         
 
-        button_Listar = Button(window, text="Listar Funcionarios")
-        button_Listar.pack()
+    button_Listar = Button(window, text="Listar Funcionarios")
+    button_Listar.pack()
 
-        button_atualizar = Button(window, text="Atualizar/Corrigir Cadastro do funcionario")
-        button_atualizar.pack()
+    button_atualizar = Button(window, text="Atualizar/Corrigir Cadastro do funcionario")
+    button_atualizar.pack()
 
-        button_deletar = Button(window, text="Deletar Funcionario")
-        button_deletar.pack()
+    button_deletar = Button(window, text="Deletar Funcionario")
+    button_deletar.pack()
 
-        button_sair = Button(window, text="Sair")
-        button_sair.pack()
-
-
-        button_Cadastro.config(command = sistema.cadastrar_funcionarios)
-        button_Listar.config(command = sistema.Listar_Funcionarios)
-        button_atualizar.config(command = sistema.Atualizar_Funcionario)
-        button_deletar.config(command = sistema.Deletar_Funcionario)
-        button_sair.config(command = self.sair)
-
-        window.mainloop()  
+    button_sair = Button(window, text="Sair")
+    button_sair.pack()
 
 
+    button_Cadastro.config(command = sistema.cadastrar_funcionarios)
+    button_Listar.config(command = sistema.Listar_Funcionarios)
+    button_atualizar.config(command = sistema.Atualizar_Funcionario)
+    button_deletar.config(command = sistema.Deletar_Funcionario)
+    button_sair.config(command = self.sair)
+
+    window.mainloop()
+
+
+    def Atualizar():
+        ID = entry_ID.get()
+        Name = entry_Name.get()
+        CPF = entry_CPF.get()
+        Data_nascimento = entry_Data_nascimento.get()
+        Rua = entry_Rua.get()
+        Cidade = entry_Cidade.get()
+        Estado = entry_Estado.get()
+        Cep = entry_Cep.get()
+        Inicio_contrato = entry_InicioContrato.get()
+        Fim_contrato = entry_FimContrato.get()
+        Salario = entry_Salario.get()
+        Cargo = entry_Cargo.get()
+
+    def Deletar():
+        ID = entry_ID.get()
+        Name = entry_Name.get()
 
     def sair(self):
         print("Até Mais :) ")
@@ -49,7 +65,111 @@ class Ponto_Server():
 
 
 
+class Cadastro_page():
+    servidor = cadastro.Server()
+    sistema = cadastro.Funcionario(servidor)
 
+    window = Tk()
+    window.geometry("600x600")
+    window.title("Pagina de controle de informação dos funcionarios ")
+    window.config(background="#2545d3")
+
+    label = Label(window, text='Vamos Cadastraro o funcionario')
+    label.pack()
+
+    Label(window, text="Nome").grid(row=0, column=0)
+    entry_Name = Entry(window)
+    entry_Name.grid(row=0, column=1)
+
+    Label(window, text="CPF").grid(row=1, column=0)
+    entry_cpf = Entry(window)
+    entry_cpf.grid(row=1, column=1)
+
+    Label(window, text="Data de Nascimento").grid(row=2, column=0)
+    entry_DataNascimento = Entry(window)
+    entry_DataNascimento.grid(row=2, column=1)
+
+    Label(window, text="RUA").grid(row=3, column=1)
+    entry_Rua = Entry(window)
+    entry_Rua.grid(row=3, column=1)
+
+    Label(window, text="Cidade").grid(row=4, column=0)
+    entry_Cidade = Entry(window)
+    entry_Cidade.grid(row=4, column=1)
+
+    Label(window, text="Estado").grid(row=5, column=0)
+    entry_Estado = Entry(window)
+    entry_Estado.grid(row=5, column=1)
+
+    Label(window, text="Cep").grid(row=6, column=0)
+    entry_Cep = Entry(window)
+    entry_Cep.grid(row=6, column=1)
+
+    Label(window, text="Data do Inicio do Contrato").grid(row=7, column=0)
+    entry_InicioContrato = Label(window)
+    entry_InicioContrato.grid(row=7, column=1)
+
+    Label(window, text="Data do Fim do Contrato").grid(row=8, column=0)
+    entry_FimContrato = Label(window)
+    entry_FimContrato.grid(row=8, column=1)
+
+    Label(window, text="Salario").grid(row=9, column=0)
+    entry_Salario = Entry(window)
+    entry_Salario.grid(row=9, column=1)
+
+    Label(window, text="Cargo").grid(row=10, column=0)
+    entry_Cargo = Entry(window)
+    entry_Cargo.grid(row=10, column=1)
+
+    def Cadastrar(self):
+        Name = self.entry_Name.get()
+        CPF = self.entry_CPF.get()
+        Data_nascimento = self.entry_DataNascimento.get()
+        Rua = self.entry_Rua.get()
+        Cidade = self.entry_Cidade.get()
+        Estado = self.entry_Estado.get()
+        Cep = self.entry_Cep.get()
+        Inicio_contrato = self.entry_InicioContrato.get()
+        Fim_contrato = self.entry_FimContrato.get()
+        Salario = self.entry_Salario.get()
+        Cargo = self.entry_Cargo.get()
+
+        self.sistema(Name, CPF, Data_nascimento, Rua, Cidade, Estado, Cep, Inicio_contrato, Fim_contrato, Salario, Cargo)
+    
+    window.mainloop()
+
+
+class Atualizar_page():
+    servidor = cadastro.Server()
+    sistema = cadastro.Funcionario(servidor)
+
+    window = Tk()
+    window.geometry("600x600")
+    window.title("Pagina de controle de informação dos funcionarios ")
+    window.config(background="#2545d3")
+
+    label = Label(window, text='Escolha uma opção: ')
+    label.pack()
+
+
+
+    window.mainloop()
+
+
+class Deletar_page():
+    servidor = cadastro.Server()
+    sistema = cadastro.Funcionario(servidor)
+
+    window = Tk()
+    window.geometry("600x600")
+    window.title("Pagina de controle de informação dos funcionarios ")
+    window.config(background="#2545d3")
+
+    label = Label(window, text='Escolha uma opção: ')
+    label.pack()
+
+
+    window.mainloop()
 
 
 if __name__ == "__main__":
--- cadastro.py
+++ cadastro.py
@@ -6,34 +6,47 @@ class Server():
     def __init__(self, db_path='ponto.db'):
         self.db_path = db_path
 
+class Endereço():
+    def __init__(self, rua, cidade, estado, cep):
+        self.Rua = rua
+        self.Cidade = cidade
+        self.Estado = estado
+        self.Cep = cep
+    
+    def __str__(self):
+        return f"{self.Rua}, {self.Cidade} - {self.Estado}, {self.Cep}"
+
+
 class Funcionario():
     def __init__(self, server):
         self.server = server
 
-    def cadastrar_funcionarios(self):
+    def cadastrar_funcionarios(self, name, cpf, data_nascimento, endereço, inicio_contrato, fim_contrato, salario, cargo):
+    
+        self.Name = name
+        self.CPF = cpf
+        self.Data_nascimento = data_nascimento
+        self.Endereço = endereço
+        self.Inicio_contrato = inicio_contrato
+        self.Fim_contrato = fim_contrato
+        self.Salario = salario
+        self.Cargo = cargo
+
         with sqlite3.connect(self.server.db_path) as connection:
             cursor = connection.cursor()
 
-            res = 'sim'
-    
-            while res.lower() == 'sim':
-                Name = input('Escreva seu nome: ')
-                data_nascimento = input('Digite sua data de nascimento: ')
-                cargo = input('Qual é o seu Cargo: ')
 
-                insert_query = '''
-                    INSERT INTO FUNCIONARIO (NAME, DATA_NASCIMENTO, CARGO)
-                    VALUES (?, ?, ?)
-                '''
+            insert_query = '''
+                INSERT INTO FUNCIONARIO (NAME, CPF, DATA_NASCIMENTO, ENDEREÇO, INICIO_DO_CONTRATO, FIM_DO_CONTRATO, SALARIO, CARGO)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            '''
 
-                funcionario_inf = (Name, data_nascimento, cargo)
+            funcionario_inf = (name, cpf, data_nascimento, str(endereço), inicio_contrato, fim_contrato, salario, cargo)
 
-                cursor.execute(insert_query, funcionario_inf)
-                connection.commit()
+            cursor.execute(insert_query, funcionario_inf)
+            connection.commit()
 
-                print('Funcionário Cadastrado com Sucesso')
-
-                res = input('Ainda tem mais funcionarios para cadastrar? (sim/nao): ')
+            print('Funcionário Cadastrado com Sucesso')
 
     def Listar_Funcionarios(self):
         with sqlite3.connect(self.server.db_path) as connection:
@@ -50,50 +63,54 @@ class Funcionario():
             for funcionario in funcionarios:
                 print(funcionario)
     
-    def Atualizar_Funcionario(self):
-        res = 'sim'
-    
-        while res.lower() == 'sim':
-            with sqlite3.connect(self.server.db_path) as connection:
-                cursor = connection.cursor()
+    def Atualizar_Funcionario(self, ID, name, cpf, data_nascimento, endereço, inicio_contrato, fim_contrato, salario, cargo):
+        
+        self.ID = ID
+        self.Name = name
+        self.CPF = cpf
+        self.Data_nascimento = data_nascimento
+        self.Endereço = endereço
+        self.Inicio_contrato = inicio_contrato
+        self.Fim_contrato = fim_contrato
+        self.Salario = salario
+        self.Cargo = cargo
 
-                update_query = '''
-                UPDATE FUNCIONARIO
-                SET NAME = ?, DATA_NASCIMENTO = ?, CARGO = ?
-                WHERE ID_FUNCIONARIO = ?;
-                '''
+        with sqlite3.connect(self.server.db_path) as connection:
+            cursor = connection.cursor()
 
-                id = int(input("Qual é o ID do funcionario que quer modificar? "))
-                nome = input("Corrigir o nome: ")
-                nascimento = input("Corrigir Data de nascimento: ")
-                cargo = input("Qual é o Cargo atualizado: ")
+            update_query = '''
+            UPDATE FUNCIONARIO
+            SET NAME = ?, CPF, DATA_NASCIMENTO = ?, ENDEREÇO, INICIO_DO_CONTRATO, FIM_DO_CONTRATO, SALARIO CARGO = ?
+            WHERE ID_FUNCIONARIO = ?;
+            '''
 
-                cursor.execute(update_query, (nome, nascimento, cargo, id))
+            print(f"Qual o ID {ID}")
 
-                connection.commit()
+            cursor.execute(update_query, (name, cpf, data_nascimento, str(endereço), inicio_contrato, fim_contrato, salario, cargo, ID))
 
-                print(f"Funcionario {nome} foi atualizado/corrigido")
+            connection.commit()
 
-                res = str(input("Tem mais alguem para atualizar?(sim/nao) "))
+            print(f"Funcionario {name} foi atualizado/corrigido")
 
-    def Deletar_Funcionario(self):
-        res = "sim"
+    def Deletar_Funcionario(self, ID, name):
+        
+        self.ID = ID
+        self.Name = name
 
-        while res.lower() == "sim":
-            with sqlite3.connect(self.server.db_path) as connection:
-                cursor = connection.cursor()
+        with sqlite3.connect(self.server.db_path) as connection:
+            cursor = connection.cursor()
 
-                delete_query = '''
-                DELETE FROM FUNCIONARIO
-                WHERE ID_FUNCIONARIO = ?;
-                '''
+            delete_query = '''
+            DELETE FROM FUNCIONARIO
+            WHERE ID_FUNCIONARIO = ?;
+            '''
 
-                id = int(input("Qual é o ID do funcionario que quer remover? "))
-                nome = str(input("Qual o nome do funcionario? "))
+            ID = int(input("Qual é o ID do funcionario que quer remover? "))
+            name = str(input("Qual o nome do funcionario? "))
 
-                cursor.execute(delete_query, (id,))
+            cursor.execute(delete_query, (ID,))
 
-                connection.commit()
+            connection.commit()
 
-                print(f"Funcionario {nome} Deletado")
-                res = str(input("Quer Deletar algum mais funcionario:(sim/nao) "))
+            print(f"Funcionario {name} Deletado")
+            res = str(input("Quer Deletar algum mais funcionario:(sim/nao) "))
